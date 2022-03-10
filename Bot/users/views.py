4ddd9fefe82354043313736be36e242033dcee65
bhakti_s_j <bhaktijayannache@gmail.com>
2022-03-10 21:16:51 +0530

    first commit

---
+++ Bot/users/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+    return render(request, 'users/home.html')
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            fs= form.save(commit=False)
+            fs.user= request.user
+            fs.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hi {username}, your account was created successfully')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'users/register.html', {'form': form})
+
+
+@login_required()
+def profile(request):
+    return render(request, 'users/profile.html')
+
+def index(response, id):
+	user = UserInfo.objects.get(id=id)
+	return render(response, "main/dashboard.html", {"user":user})
